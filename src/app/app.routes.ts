e9df499ba31845cec6a3e9b70beda36fbe900a11
myroslavSB <myroslav.sobchyshyn@px.com>
2024-11-16 16:07:39 +0100

    added content to landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import {LandingPageComponent} from "./pages/landing-page/landing-page.component";
+import {BattlePageComponent} from "./pages/battle-page/battle-page.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    loadComponent: () => LandingPageComponent,
+
+  },
+  {
+    path: "battle",
+    loadComponent: () => BattlePageComponent,
+  }
+];
